480adb883eed3d9000308650cafc9b65c716848b
chujun <985145157@qq.com>
2024-07-13 18:19:55 +0800

    chart3

---
+++ asycio_learn/chat3/01_simple_server.py
@@ -0,0 +1,27 @@
+# use command `telnet localhost 8000` to be a client
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ('127.0.0.1', 8000)
+server_socket.bind(server_address)
+server_socket.listen()
+
+try:
+    connection, address = server_socket.accept()
+    print('Connection address:', address)
+
+    buffer = b''
+
+    while buffer[-2:] != b'\r\n':
+        data = connection.recv(2)
+        if not data:
+            break
+        else:
+            print(f'I got data :{data}!')
+            buffer += data
+    print(f'All the data is {buffer}')
+    connection.sendall(buffer)
+finally:
+    server_socket.close()
